9202d091c9f1e4c4d09bd07894b1b32ee39d45e5
hogehiko <takrockjp@gmail.com>
2020-02-11 23:13:48 +0900

    Create parser.ts

---
+++ src/parser.ts
@@ -0,0 +1,63 @@
+class Def{
+    parse(buf:InputBuffer){
+
+    }
+}
+
+class Token extends Def{
+    constructor(symbol: string, regexp:RegExp){
+        super()
+    }
+}
+
+class StdNode extends Def{
+    constructor(symbol: string, arg: Def[]){
+        super()
+    }
+}
+
+class Choice extends Def{
+    constructor(arg: Def[]){
+        super()
+    }
+}
+
+class Repeat extends Def{
+    constructor(arg: Def[]){
+        super()
+    }
+}
+
+function token(symbol: string, regexp: RegExp): Token{
+    return new Token(symbol, regexp)
+}
+
+function exp(symbol: string,...children: Def[]): Token{
+    return new StdNode(symbol, children)
+}
+
+function choice(...choice: Def[]): Token{
+    return new Choice(choice)
+}
+
+function repeat(...repeat: Def[]): Token{
+    return new Repeat(repeat)
+}
+
+let number = token('number', /0-9]+/)
+let addsub_op = token('addsub_op', /[+\\-]/)
+let muldiv_op = token('muldiv_op', /[*/]/)
+let muldiv_exp = exp('muldiv_exp', number, repeat(muldiv_op, number))
+let addsub_exp = exp('addsub_exp', muldiv_exp, repeat(addsub_op, muldiv_exp))
+
+class InputBuffer{
+    ws: RegExp = /[ ]+/
+    constructor(inputString: string){
+
+    }
+}
+
+function calc(inputString: string){
+    let buf = new InputBuffer(inputString)
+    addsub_exp.parse(buf)
+}
